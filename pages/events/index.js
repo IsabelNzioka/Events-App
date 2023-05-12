9059b62337ad9d7df648e1c75112a59b7f73f9e6
IsabelNzioka <isabelnzioka@gmail.com>
2023-05-12 19:23:03 +0300

    file-based routing and stying nextjs

---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+// import React from "react";
+import Link from "next/link";
+
+const AllEventsPage = () => {
+  return (
+    <div>
+      <h1>Show all Events</h1>
+    </div>
+  );
+};
+
+export default AllEventsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,15 @@
-// import Head from "next/head";
-// import Image from "next/image";
-// import { Inter } from "@next/font/google";
-// import styles from "../styles/Home.module.css";
-
-// const inter = Inter({ subsets: ["latin"] });
-
 // import React from "react";
 import Link from "next/link";
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
       <h1>Hello Next World</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
+      <EventList items={featuredEvents} />
     </div>
   );
 };
