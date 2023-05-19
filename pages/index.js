2d5fee00288893c70239ad8b0404eadfbed7344a
IsabelNzioka <isabelnzioka@gmail.com>
2023-05-19 17:41:28 +0300

    adding static site generation on the Home page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,29 @@
 // import React from "react";
 import Link from "next/link";
-import { getFeaturedEvents } from "../dummy-data";
+// import { getFeaturedEvents } from "../dummy-data";
+
+import { getFeaturedEvents } from "../helpers/api-utils";
 import EventList from "../components/events/event-list";
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
+const HomePage = (props) => {
+  // const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
       {/* <h1>Featured Events</h1> */}
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
+}
 
 export default HomePage;
